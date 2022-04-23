dbf160c6227923cb8bfacfb35622ddfd267841ae
Ashish Tiwari <ashish@novo.cloud>
2022-04-23 19:23:45 +0530

    build err

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,13 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Workspaces } from "../dbConnection";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   const wrkspaces = await Workspaces.findAll();
 
